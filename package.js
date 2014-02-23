9d4e9c3a89e4472bbb915cc0a56b5d02f61035c5
Janpo Pan <janpo.pan@gmail.com>
2014-02-24 02:19:41 +0800

    update smart.json

--- package.js
+++ package.js
@@ -5,9 +5,3 @@ Package.describe({
 Package.on_use(function (api, where) {
   api.add_files(['FlipClock/compiled/flipclock.css', 'FlipClock/compiled/flipclock.min.js'], ['client']);
 });
-
-Package.on_test(function (api) {
-  api.use('meteor-flipclockjs');
-
-  api.add_files('meteor-flipclockjs_tests.js', ['client', 'server']);
-});
